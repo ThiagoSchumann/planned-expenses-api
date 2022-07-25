e2be3874855e357054abd9d9270d14d527fdcb55
Thiago Artur Schumann <thiagoarturschumann@gmail.com>
2022-07-25 13:29:56 -0300

    configure admin

--- api/admin/admin.py
+++ api/admin/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+models = apps.get_models()
+
+exclude_models = []
+
+for model in models:
+    try:
+        if model not in exclude_models:
+            admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
